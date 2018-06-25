e4304aaa5d683a0a65796f462b87407cd5040614
YOONAH KIM <37688086+seoulblanc@users.noreply.github.com>
2018-06-25 18:16:36 +0900

    Update corTest.R

--- corTest.R
+++ corTest.R
@@ -1,27 +1,21 @@
 
-setwd("C:/seoul_bigdata/201803시간대별csv")
-data <- read.table("21_24.csv", header=T, 
+setwd("C:/seoul_bigdata/201711시간대별csv")
+data <- read.table("allday.csv", header=T, 
                      na.strings = "NA", fileEncoding="UTF-16LE", sep = ",")
+data10 <- subset(data, select=c(편의점, 미용실, 호프간이주점, 
+        제과점, 패스트푸드점, 분식집, 양식집, 일식집, 한식음식점,커피음료))
 
-data6 <- data[ , 32:40]
-data6 <- na.omit(data6)
+data10 <- na.omit(data10)
+data10 <- data10[c(order(-data10$커피음료)),]
+data10 <- data10[-c(1),]
+#data10 <- data10[-c(1,2,3),]
 
-table1 <- cor(data1)
-table2 <- cor(data2)
-table3 <- cor(data3)
-table4 <- cor(data4)
-table5 <- cor(data5)
-table6 <- cor(data6)
+head(data10)
+dim(data10)
 
-table_all <- rbind(table1, table2, table3, table4, table5, table6)
-table_all
+cor_data_result <- cor(data10)
+coffee <- cor_data_result[,'커피음료']
+coffee <- sort(coffee, decreasing = T)
+coffee
 
-write.csv(table_all, file="time_table_all.csv", row.names=TRUE)
-
-#data11 <- subset(data, select=c(중국집,화장품))
-#data11 <- na.omit(data11)
-#data11
-
-#plot(중국집~커피음료, dat=data11)
-#fit <- lm(중국집~커피음료, dat=data11)
-#summary(fit)
+write.csv(data10, file="201711_9category.csv", row.names=TRUE)
